05c0c638b58f0e08fc38f135dc9d514d8cb2f22e
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-05-17 15:20:36 -0300

    initial commit

---
+++ src/main/java/pedroleonez/spring_introduce/SpringIntroduceApplication.java
@@ -0,0 +1,13 @@
+package pedroleonez.spring_introduce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroduceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntroduceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pedroleonez/spring_introduce/SpringIntroduceApplicationTests.java
@@ -0,0 +1,13 @@
+package pedroleonez.spring_introduce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroduceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
