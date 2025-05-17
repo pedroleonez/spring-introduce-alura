483e63f3012f9957de57889f88ab4b2454fe3fc4
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-05-17 15:24:37 -0300

    feat: add CommandLineRunner

--- src/main/java/pedroleonez/spring_introduce/SpringIntroduceApplication.java
+++ src/main/java/pedroleonez/spring_introduce/SpringIntroduceApplication.java
@@ -1,13 +1,18 @@
 package pedroleonez.spring_introduce;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringIntroduceApplication {
+public class SpringIntroduceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIntroduceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello, Spring Boot!");
+	}
 }
