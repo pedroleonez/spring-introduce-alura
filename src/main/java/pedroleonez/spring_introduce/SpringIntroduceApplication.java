539af4f1b32522f17aaf4cfe82aeb5f4b0c0d907
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-05-18 15:33:39 -0300

    feat: add ConsumeApi and DataConverter

--- src/main/java/pedroleonez/spring_introduce/SpringIntroduceApplication.java
+++ src/main/java/pedroleonez/spring_introduce/SpringIntroduceApplication.java
@@ -3,6 +3,9 @@ package pedroleonez.spring_introduce;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pedroleonez.spring_introduce.model.MovieData;
+import pedroleonez.spring_introduce.service.ConsumeApi;
+import pedroleonez.spring_introduce.service.DataConverter;
 
 @SpringBootApplication
 public class SpringIntroduceApplication implements CommandLineRunner {
@@ -13,6 +16,12 @@ public class SpringIntroduceApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello, Spring Boot!");
+		var consumeApi = new ConsumeApi();
+		var json = consumeApi.getData("https://www.omdbapi.com/?i=tt3896198&apikey=a06071ca");
+		System.out.println(json);
+
+		DataConverter dataConverter = new DataConverter();
+		MovieData movieData = dataConverter.getData(json, MovieData.class);
+		System.out.println(movieData);
 	}
 }
